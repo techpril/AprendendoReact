9b8912077ca097f89c8976b26df91970d983d409
techpril <priscila.teodorio2015@gmail.com>
2023-02-18 10:47:26 -0300

    Componentes e propriedades

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from './components/RepositoryList';
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <RepositoryList />
+    </div>
+  )
+}
+
---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,13 @@
+
+export function RepositoryItem(props) {
+    return (
+      <li>
+         <strong>{props.repository}</strong>
+         <p>Forms in React</p>
+
+         <a href="">
+               Acessar repositório
+         </a>
+      </li>
+   )
+}
